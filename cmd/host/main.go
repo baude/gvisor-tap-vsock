15083d69bb8753a922bd30197d4da72fbf9c8694
Brent Baude <bbaude@redhat.com>
2021-05-04 10:29:36 -0500

    wip
    Signed-off-by: Brent Baude <bbaude@redhat.com>

--- cmd/host/main.go
+++ cmd/host/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"net"
 	"net/http"
 	"net/http/pprof"
+	"net/url"
+	"os"
+	"os/signal"
 	"regexp"
+	"strconv"
+	"syscall"
 	"time"
 
 	"github.com/code-ready/gvisor-tap-vsock/pkg/transport"
@@ -23,15 +29,22 @@ var (
 	endpoints    arrayFlags
 	vpnkitSocket string
 	qemuSocket   string
+	sshPort      int
+	pidFile      string
 )
 
 func main() {
-	flag.Var(&endpoints, "listen", fmt.Sprintf("url where the tap send packets (default %s)", transport.DefaultURL))
+	flag.Var(&endpoints, "listen", fmt.Sprintf("Url where the tap send packets (default %s)", transport.DefaultURL))
 	flag.BoolVar(&debug, "debug", false, "debug")
 	flag.IntVar(&mtu, "mtu", 1500, "mtu")
+	flag.IntVar(&sshPort, "ssh-port", 2222, "Port to access the guest virtual machine. Must be between 1024 and 65535")
 	flag.StringVar(&vpnkitSocket, "listen-vpnkit", "", "VPNKit socket to be used by Hyperkit")
 	flag.StringVar(&qemuSocket, "listen-qemu", "", "Socket to be used by Qemu")
+	flag.StringVar(&pidFile, "pid-file", "", "Generate a file with the PID in it")
 	flag.Parse()
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
 
 	if debug {
 		log.SetLevel(log.DebugLevel)
@@ -41,16 +54,45 @@ func main() {
 		endpoints = append(endpoints, transport.DefaultURL)
 	}
 
-	if vpnkitSocket != "" && qemuSocket != "" {
-		log.Fatal("cannot use qemu and vpnkit protocol at the same time")
+	if _, err := url.Parse(qemuSocket); err != nil {
+		exitWithError(errors.New("invalid value for listen-qemu"))
 	}
 
+	if vpnkitSocket != "" && qemuSocket != "" {
+		exitWithError(errors.New("cannot use qemu and vpnkit protocol at the same time"))
+	}
+	// If the given port is not between the privileged ports
+	// and the oft considered maximum port, return an error.
+	if sshPort < 1024 || sshPort > 65535 {
+		exitWithError(errors.New("ssh-port value must be between 1024 and 65535"))
+	}
 	protocol := types.HyperKitProtocol
 	if qemuSocket != "" {
 		protocol = types.QemuProtocol
 	}
 
-	if err := run(&types.Configuration{
+	// Create a PID file if requested
+	if len(pidFile) > 0 {
+		f, err := os.Create(pidFile)
+		if err != nil {
+			exitWithError(err)
+		}
+		defer func() {
+			fmt.Println("11111111111111111111111111111111111111")
+			if err := os.Remove(pidFile); err != nil {
+				log.Error(err)
+			}
+		}()
+		pid := os.Getppid()
+		if _, err := f.WriteString(strconv.Itoa(pid)); err != nil {
+			exitWithError(err)
+		}
+	}
+
+	// Catch signals so exits are graceful and defers can run
+	closeHandler(ctx, cancel)
+	fmt.Println("22222222222222")
+	if err := run(ctx, cancel, &types.Configuration{
 		Debug:             debug,
 		CaptureFile:       captureFile(),
 		MTU:               mtu,
@@ -92,7 +134,7 @@ func main() {
 			},
 		},
 		Forwards: map[string]string{
-			":2222": "192.168.127.2:22",
+			fmt.Sprintf(":%s", strconv.Itoa(sshPort)): "192.168.127.2:22",
 		},
 		NAT: map[string]string{
 			"192.168.127.254": "127.0.0.1",
@@ -102,7 +144,7 @@ func main() {
 		},
 		Protocol: protocol,
 	}, endpoints); err != nil {
-		log.Fatal(err)
+		exitWithError(err)
 	}
 }
 
@@ -124,7 +166,7 @@ func captureFile() string {
 	return "capture.pcap"
 }
 
-func run(configuration *types.Configuration, endpoints []string) error {
+func run(ctx context.Context, cancel context.CancelFunc, configuration *types.Configuration, endpoints []string) error {
 	vn, err := virtualnetwork.New(configuration)
 	if err != nil {
 		return err
@@ -140,16 +182,23 @@ func run(configuration *types.Configuration, endpoints []string) error {
 		}
 
 		go func() {
-
-			if err := http.Serve(ln, withProfiler(vn)); err != nil {
+			if err := http.Serve(ln, withProfiler(vn)); err != nil && err != http.ErrServerClosed {
 				errCh <- err
+				fmt.Println("8")
+				return
 			}
 		}()
 	}
+	// TODO: only run when debug?
 	go func() {
 		for {
-			fmt.Printf("%v sent to the VM, %v received from the VM\n", humanize.Bytes(vn.BytesSent()), humanize.Bytes(vn.BytesReceived()))
-			time.Sleep(5 * time.Second)
+			select {
+			case <-time.After(5 * time.Second):
+				fmt.Printf("%v sent to the VM, %v received from the VM\n", humanize.Bytes(vn.BytesSent()), humanize.Bytes(vn.BytesReceived()))
+			case <-ctx.Done():
+				fmt.Println("7")
+				return
+			}
 		}
 	}()
 
@@ -160,6 +209,12 @@ func run(configuration *types.Configuration, endpoints []string) error {
 		}
 		go func() {
 			for {
+				select {
+				case <-ctx.Done():
+					fmt.Println("6")
+					return
+				default:
+				}
 				conn, err := vpnkitListener.Accept()
 				if err != nil {
 					log.Errorf("vpnkit accept error: %s", err)
@@ -179,12 +234,23 @@ func run(configuration *types.Configuration, endpoints []string) error {
 		if err != nil {
 			return err
 		}
+		defer func() {
+			if err := qemuListener.Close(); err != nil {
+				log.Errorf("error closing %s: %q", qemuSocket, err)
+			}
+		}()
 		go func() {
 			for {
+				select {
+				case <-ctx.Done():
+					fmt.Println("5")
+					return
+				default:
+				}
 				conn, err := qemuListener.Accept()
 				if err != nil {
 					log.Errorf("qemu accept error: %s", err)
-					continue
+					cancel()
 				}
 				go func() {
 					if err := vn.AcceptQemu(conn); err != nil {
@@ -204,8 +270,9 @@ func run(configuration *types.Configuration, endpoints []string) error {
 		mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 			_, _ = writer.Write([]byte(`Hello world!\n`))
 		})
-		if err := http.Serve(ln, mux); err != nil {
+		if err := http.Serve(ln, mux); err != nil && err != http.ErrServerClosed {
 			errCh <- err
+			return
 		}
 	}()
 	return <-errCh
@@ -220,4 +287,20 @@ func withProfiler(vn *virtualnetwork.VirtualNetwork) http.Handler {
 		mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	}
 	return mux
+}
+
+func exitWithError(err error) {
+	log.Error(err)
+	os.Exit(1)
+}
+
+func closeHandler(ctx context.Context, cancelFunc context.CancelFunc) {
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sigChan
+		fmt.Println("3")
+		cancelFunc()
+		fmt.Println("4")
+	}()
 }
